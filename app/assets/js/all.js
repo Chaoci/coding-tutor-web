13d805bbb1e40af5bacaf70ef94d304f36b06187
Chaoci <charlieischao@gmail.com>
2022-08-15 21:52:45 +0800

    second-section

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,29 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
+// const menu = document.getElementById('menu')
+// const nav_mobile = document.querySelector('.navbar-mobile');
+
+// meun.addEventListener('click',()=>{
+//   nav_mobile.classList.toggle('.d-block')
+// });
+
+// const mean = document.querySelector('.menu');
+// var div = document.querySelector("navbar-mobile");
+// var cssCollection = window.getComputedStyle(div, null);
+// var result = cssCollection.getPropertyValue("display"); //利用js取css值
+
+// $(function () {
+// 	$(".menu i").onclick(function (e) {
+// 		e.preventDefault();
+// 		//h2 在父層元素中，找到 p 元素，並且給它滑動效果
+// 		$('.slide').parent().find("li").slideToggle(700);
+// 		//h2 在父層 .question 元素中，找到其他 .question 同層元素，再找到該同層元素的 p 標籤，並向上收闔
+// 		$('.slide').parent().siblings().find("li").slideUp(700);
+// 	});
+
+// 	//如果有出現menu icon則為block，表示正在手機畫面則執行以下code
+// 	if(result == 'block'){  
+// 		$(".slide li").click(function(e){
+// 			$('.slide').parent().find("li").slideToggle(700);
+// 			$('.slide').parent().siblings().find("li").slideUp(700);
+// 		})
+// 	}
+// });
