6fefc45e25fd1b75136aec193ff1c1b76b51777c
Chaoci <charlieischao@gmail.com>
2022-08-19 11:43:04 +0800

    Update 2022-08-19T03:43:02.063Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,59 +1,42 @@
 "use strict";
 
-// const menu = document.getElementById('menu')
-// const nav_mobile = document.querySelector('.navbar-mobile');
-// meun.addEventListener('click',()=>{
-//   nav_mobile.classList.toggle('.d-block')
-// });
-// const mean = document.querySelector('.menu');
-// var div = document.querySelector("navbar-mobile");
-// var cssCollection = window.getComputedStyle(div, null);
-// var result = cssCollection.getPropertyValue("display"); //利用js取css值
-// $(function () {
-// 	$(".menu i").onclick(function (e) {
-// 		e.preventDefault();
-// 		//h2 在父層元素中，找到 p 元素，並且給它滑動效果
-// 		$('.slide').parent().find("li").slideToggle(700);
-// 		//h2 在父層 .question 元素中，找到其他 .question 同層元素，再找到該同層元素的 p 標籤，並向上收闔
-// 		$('.slide').parent().siblings().find("li").slideUp(700);
-// 	});
-// 	//如果有出現menu icon則為block，表示正在手機畫面則執行以下code
-// 	if(result == 'block'){  
-// 		$(".slide li").click(function(e){
-// 			$('.slide').parent().find("li").slideToggle(700);
-// 			$('.slide').parent().siblings().find("li").slideUp(700);
-// 		})
-// 	}
-// });
-// const swiper = new Swiper('.swiper', {
-//     // Optional parameters
-//     direction: 'vertical',
-//     loop: true,
-//     // If we need pagination
-//     pagination: {
-//       el: '.swiper-pagination',
-//     },
-//     // Navigation arrows
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-//     // And if we need scrollbar
-//     scrollbar: {
-//       el: '.swiper-scrollbar',
-//     },
-//   });
-var swiper = new Swiper(".mySwiper", {
-  effect: 'fade',
-  fadeEffect: {
-    crossFade: true
-  },
+var swiper = new Swiper(".course-swiper", {
   autoplay: {
-    delay: 3000
+    disableOnInteraction: false,
+    delay: 0
   },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true
+  speed: 1500,
+  loop: true,
+  spaceBetween: 16,
+  slidesPerView: 3,
+  slidesPerGroup: 1,
+  centeredSlides: true,
+  //多欄
+  breakpoints: {
+    768: {
+      slidesPerView: 6
+    },
+    992: {
+      slidesPerView: 6
+    },
+    1200: {
+      slidesPerView: 8
+    },
+    1300: {
+      slidesPerView: 9
+    }
   }
-});
+}); // var swiper2 = new Swiper(".mySwiper", {
+//     effect:'fade',
+//     fadeEffect:{
+//         crossFade:true
+//     },
+//     autoplay: {
+//         delay: 2000,
+//     },
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     },
+// });
 //# sourceMappingURL=all.js.map
